3ecd4849313962e35f15059627067714ff3f0907
Dev-SardarAbdul <dev.sardarabdul@gmail.com>
2023-04-29 11:47:41 +0500

    About Us added

--- frontend/src/components/homeCom/aboutUs/index.tsx
+++ frontend/src/components/homeCom/aboutUs/index.tsx
@@ -4,7 +4,29 @@ import {
   AboutUsSubText,
   AboutUsWrapper,
   BtnDiv,
+  SwiperWrapper,
 } from "./styles";
+import {
+  aboutUs1,
+  aboutUs2,
+  aboutUs3,
+  aboutUs4,
+  aboutUs5,
+  aboutUs6,
+} from "assets";
+import "swiper/modules/effect-coverflow/effect-coverflow.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+  EffectCoverflow,
+} from "swiper";
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]);
 
 function AboutUS() {
   return (
@@ -18,6 +40,51 @@ function AboutUS() {
           ourselves on our commitment to quality, efficiency, and customer
           satisfaction.
         </AboutUsSubText>
+        <SwiperWrapper>
+          <Swiper
+            // activeSlideKey='2'
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            autoplay={{ delay: 1000, disableOnInteraction: false }}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 100,
+              modifier: 2,
+              slideShadows: true,
+            }}
+            // scrollbar={{draggable:true}}
+            navigation={true}
+            initialSlide={2}
+            pagination={true}
+            modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src={aboutUs1} />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src={aboutUs2} />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src={aboutUs3} />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src={aboutUs4} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={aboutUs5} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={aboutUs6} />
+            </SwiperSlide>
+          </Swiper>
+        </SwiperWrapper>
         <BtnDiv>
           <Button readMore={true}>More About Us</Button>
         </BtnDiv>
--- frontend/src/components/homeCom/aboutUs/styles.ts
+++ frontend/src/components/homeCom/aboutUs/styles.ts
@@ -18,7 +18,6 @@ export const AboutUsHeader = styled.h2`
   text-transform: capitalize;
   color: #ee6700;
   text-align: center;
-  position: relative;
 `;
 
 export const AboutUsSubText = styled.h4`
@@ -29,6 +28,11 @@ export const AboutUsSubText = styled.h4`
   text-transform: capitalize;
   color: #484848;
   text-align: center;
+
+  @media (max-width: 575.98px) {
+    text-align: justify;
+    text-align-last: center;
+  }
 `;
 
 export const BtnDiv = styled.div`
@@ -36,4 +40,93 @@ export const BtnDiv = styled.div`
   justify-content: center;
   align-items: center;
   margin: 2rem 0;
+`;
+
+export const SwiperWrapper = styled.div`
+  .swiper {
+    width: 100%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+
+  .swiper-slide {
+    background-position: center;
+    background-size: cover;
+    width: 300px;
+    /* height: 300px; */
+    filter: blur(4px);
+  }
+
+  .swiper-slide-active {
+    filter: blur(0px);
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 350px;
+    border: none;
+  }
+
+  .swiper-3d .swiper-slide-shadow-left {
+    background-image: none;
+  }
+
+  .swiper-3d .swiper-slide-shadow-right {
+    background-image: none;
+  }
+
+  .swiper-button-next {
+    /* background-image: url(); */
+    margin-right: 10rem;
+    background-repeat: no-repeat !important;
+    width: 4rem;
+
+    &::after {
+      display: none;
+    }
+
+    @media (max-width: 1660px) {
+      margin-right: -2px;
+    }
+
+    @media (max-width: 1480px) {
+      margin-right: 0;
+    }
+
+    @media (max-width: 756px) {
+      display: none;
+    }
+  }
+
+  .swiper-button-prev {
+    color: black;
+    /* background-image: url(); */
+    margin-left: 10rem;
+    background-repeat: no-repeat !important;
+    width: 100% !important;
+
+    &::after {
+      display: none;
+    }
+
+    @media (max-width: 1660px) {
+      margin-left: -2px;
+    }
+
+    @media (max-width: 1480px) {
+      margin-left: 0;
+    }
+
+    @media (max-width: 756px) {
+      display: none;
+    }
+  }
+
+  .swiper-horizontal > .swiper-pagination-bullets,
+  .swiper-pagination-bullets.swiper-pagination-horizontal,
+  .swiper-pagination-custom,
+  .swiper-pagination-fraction {
+    display: none;
+  }
 `;
